c4ab7c6d11f4ef36b462442348d38d6a88ce62c2
应卓 <yingzhor@gmail.com>
2021-11-27 14:05:54 +0800

    2021-11-27 14:05:52

---
+++ src/main/java/spring/turbo/util/HexUtils.java
@@ -0,0 +1,62 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.util;
+
+/**
+ * @author 应卓
+ * @since 1.0.0
+ */
+public final class HexUtils {
+
+    private static final char[] HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f'};
+
+    private HexUtils() {
+        super();
+    }
+
+    public static char[] encode(byte[] bytes) {
+        final int nBytes = bytes.length;
+        char[] result = new char[2 * nBytes];
+
+        int j = 0;
+        for (byte aByte : bytes) {
+            // Char for top 4 bits
+            result[j++] = HEX[(0xF0 & aByte) >>> 4];
+            // Bottom 4
+            result[j++] = HEX[(0x0F & aByte)];
+        }
+
+        return result;
+    }
+
+    public static byte[] decode(CharSequence s) {
+        int nChars = s.length();
+
+        if (nChars % 2 != 0) {
+            throw new IllegalArgumentException(
+                    "Hex-encoded string must have an even number of characters");
+        }
+
+        byte[] result = new byte[nChars / 2];
+
+        for (int i = 0; i < nChars; i += 2) {
+            int msb = Character.digit(s.charAt(i), 16);
+            int lsb = Character.digit(s.charAt(i + 1), 16);
+
+            if (msb < 0 || lsb < 0) {
+                throw new IllegalArgumentException(
+                        "Detected a Non-hex character at " + (i + 1) + " or " + (i + 2) + " position");
+            }
+            result[i / 2] = (byte) ((msb << 4) | lsb);
+        }
+        return result;
+    }
+
+}
