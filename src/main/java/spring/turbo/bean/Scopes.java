dcb21c1358ef28564d36417786f9bb5e1e8c4c20
应卓 <yingzhor@gmail.com>
2021-12-20 08:21:02 +0800

    2021-12-20 08:21:00

---
+++ src/main/java/spring/turbo/bean/Scopes.java
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.bean;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+
+/**
+ * @author 应卓
+ * @since 1.0.0
+ */
+public final class Scopes {
+
+    public static final String SINGLETON = ConfigurableBeanFactory.SCOPE_SINGLETON;
+    public static final String PROTOTYPE = ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
+    private Scopes() {
+        super();
+    }
+
+}
