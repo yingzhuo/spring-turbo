b46200105008b68cb3936e4c2ff47dbc80ff4af8
应卓 <yingzhor@gmail.com>
2021-12-22 07:59:41 +0800

    2021-12-22 07:59:40

---
+++ src/main/java/spring/turbo/bean/SpringApplicationName.java
@@ -0,0 +1,24 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import java.lang.annotation.*;
+
+/**
+ * @author 应卓
+ * @since 1.0.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Value("${spring.application.name}")
+public @interface SpringApplicationName {
+}
