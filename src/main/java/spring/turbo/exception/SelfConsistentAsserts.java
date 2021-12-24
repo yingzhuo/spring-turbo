f5dac7d5cd3512bfe433bbb48c3812e1417246d5
应卓 <yingzhor@gmail.com>
2021-12-24 09:29:38 +0800

    2021-12-24 09:29:36

--- src/main/java/spring/turbo/exception/SelfConsistentAsserts.java
+++ src/main/java/spring/turbo/exception/SelfConsistentAsserts.java
@@ -8,6 +8,7 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package spring.turbo.exception;
 
+import org.springframework.lang.Nullable;
 import org.springframework.util.StringUtils;
 
 import java.util.*;
@@ -24,105 +25,105 @@ public final class SelfConsistentAsserts {
         super();
     }
 
-    public static <T> void notNull(T obj) {
+    public static <T> void notNull(@Nullable T obj) {
         notNull(obj, null);
     }
 
-    public static <T> void notNull(T obj, String message, Object... params) {
+    public static <T> void notNull(@Nullable T obj, @Nullable String message, Object... params) {
         if (obj == null) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T> void equals(T obj1, T obj2) {
+    public static <T> void equals(@Nullable T obj1, @Nullable T obj2) {
         equals(obj1, obj2, null);
     }
 
-    public static <T> void equals(T obj1, T obj2, String message, Object... params) {
+    public static <T> void equals(@Nullable T obj1, @Nullable T obj2, String message, Object... params) {
         if (!Objects.equals(obj1, obj2)) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T> void notEquals(T obj1, T obj2) {
+    public static <T> void notEquals(@Nullable T obj1, @Nullable T obj2) {
         notEquals(obj1, obj2, null);
     }
 
-    public static <T> void notEquals(T obj1, T obj2, String message, Object... params) {
+    public static <T> void notEquals(@Nullable T obj1, @Nullable T obj2, String message, Object... params) {
         if (Objects.equals(obj1, obj2)) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T> void notEmpty(Collection<T> collection) {
+    public static <T> void notEmpty(@Nullable Collection<T> collection) {
         notEmpty(collection, null);
     }
 
-    public static <T> void notEmpty(Collection<T> collection, String message, Object... params) {
+    public static <T> void notEmpty(@Nullable Collection<T> collection, String message, Object... params) {
         notNull(collection, message, params);
         if (collection.isEmpty()) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T> void notEmpty(T[] array) {
+    public static <T> void notEmpty(@Nullable T[] array) {
         notEmpty(array, null);
     }
 
-    public static <T> void notEmpty(T[] array, String message, Object... params) {
+    public static <T> void notEmpty(@Nullable T[] array, String message, Object... params) {
         notNull(array, message, params);
         if (array.length == 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void notEmpty(Map<?, ?> map) {
+    public static void notEmpty(@Nullable Map<?, ?> map) {
         notEmpty(map, null);
     }
 
-    public static void notEmpty(Map<?, ?> map, String message, Object... params) {
+    public static void notEmpty(@Nullable Map<?, ?> map, String message, Object... params) {
         notNull(map, message, params);
         if (map.isEmpty()) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T> void isPresent(Optional<T> optional) {
+    public static <T> void isPresent(@Nullable Optional<T> optional) {
         isPresent(optional, null);
     }
 
-    public static <T> void isPresent(Optional<T> optional, String message, Object... params) {
+    public static <T> void isPresent(@Nullable Optional<T> optional, String message, Object... params) {
         notNull(optional, message, params);
         if (!optional.isPresent()) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T extends CharSequence> void notEmpty(T string) {
+    public static <T extends CharSequence> void notEmpty(@Nullable T string) {
         notEmpty(string, null);
     }
 
-    public static <T extends CharSequence> void notEmpty(T string, String message, Object... params) {
+    public static <T extends CharSequence> void notEmpty(@Nullable T string, String message, Object... params) {
         if (StringUtils.hasLength(string)) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T extends CharSequence> void notBlank(T string) {
+    public static <T extends CharSequence> void notBlank(@Nullable T string) {
         notBlank(string, null);
     }
 
-    public static <T extends CharSequence> void notBlank(T string, String message, Object... params) {
+    public static <T extends CharSequence> void notBlank(@Nullable T string, String message, Object... params) {
         if (StringUtils.hasText(string)) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static <T> void noNullElement(Collection<T> collection) {
+    public static <T> void noNullElement(@Nullable Collection<T> collection) {
         noNullElement(collection, null);
     }
 
-    public static <T> void noNullElement(Collection<T> collection, String message, Object... params) {
+    public static <T> void noNullElement(@Nullable Collection<T> collection, String message, Object... params) {
         notNull(collection, message, params);
         boolean noNull = collection.stream().noneMatch(Objects::isNull);
         if (!noNull) {
@@ -130,11 +131,11 @@ public final class SelfConsistentAsserts {
         }
     }
 
-    public static <T> void noNullElement(T[] array) {
+    public static <T> void noNullElement(@Nullable T[] array) {
         notNull(array, null);
     }
 
-    public static <T> void noNullElement(T[] array, String message, Object... params) {
+    public static <T> void noNullElement(@Nullable T[] array, String message, Object... params) {
         notNull(array, message, params);
         boolean noNull = Arrays.stream(array).noneMatch(Objects::isNull);
         if (!noNull) {
@@ -162,112 +163,132 @@ public final class SelfConsistentAsserts {
         }
     }
 
-    public static void isPositive(Double number) {
+    @Deprecated
+    public static void isPositive(@Nullable Double number) {
         isPositive(number, null);
     }
 
-    public static void isPositive(Double number, String message, Object... params) {
+    @Deprecated
+    public static void isPositive(@Nullable Double number, String message, Object... params) {
         if (number == null || number <= 0.0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isPositive(Integer number) {
+    @Deprecated
+    public static void isPositive(@Nullable Integer number) {
         isPositive(number, null);
     }
 
-    public static void isPositive(Integer number, String message, Object... params) {
+    @Deprecated
+    public static void isPositive(@Nullable Integer number, String message, Object... params) {
         if (number == null || number <= 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isPositive(Long number) {
+    @Deprecated
+    public static void isPositive(@Nullable Long number) {
         isPositive(number, null);
     }
 
-    public static void isPositive(Long number, String message, Object... params) {
+    @Deprecated
+    public static void isPositive(@Nullable Long number, String message, Object... params) {
         if (number == null || number <= 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isNegative(Double number) {
+    @Deprecated
+    public static void isNegative(@Nullable Double number) {
         isNegative(number, null);
     }
 
-    public static void isNegative(Double number, String message, Object... params) {
+    @Deprecated
+    public static void isNegative(@Nullable Double number, String message, Object... params) {
         if (number == null || number >= 0.0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isNegative(Integer number) {
+    @Deprecated
+    public static void isNegative(@Nullable Integer number) {
         isNegative(number, null);
     }
 
-    public static void isNegative(Integer number, String message, Object... params) {
+    @Deprecated
+    public static void isNegative(@Nullable Integer number, String message, Object... params) {
         if (number == null || number >= 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isNegative(Long number) {
+    @Deprecated
+    public static void isNegative(@Nullable Long number) {
         isNegative(number, null);
     }
 
-    public static void isNegative(Long number, String message, Object... params) {
+    @Deprecated
+    public static void isNegative(@Nullable Long number, String message, Object... params) {
         if (number == null || number >= 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isZero(Double number) {
+    @Deprecated
+    public static void isZero(@Nullable Double number) {
         isZero(number, null);
     }
 
-    public static void isZero(Double number, String message, Object... params) {
+    @Deprecated
+    public static void isZero(@Nullable Double number, String message, Object... params) {
         if (number == null || number != 0.0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isZero(Integer number) {
+    @Deprecated
+    public static void isZero(@Nullable Integer number) {
         isZero(number, null);
     }
 
-    public static void isZero(Integer number, String message, Object... params) {
+    @Deprecated
+    public static void isZero(@Nullable Integer number, String message, Object... params) {
         if (number == null || number != 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isZero(Long number) {
+    @Deprecated
+    public static void isZero(@Nullable Long number) {
         isZero(number, null);
     }
 
-    public static void isZero(Long number, String message, Object... params) {
+    @Deprecated
+    public static void isZero(@Nullable Long number, String message, Object... params) {
         if (number == null || number != 0) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isInstanceOf(Class<?> type, Object obj) {
+    @Deprecated
+    public static void isInstanceOf(@Nullable Class<?> type, Object obj) {
         isInstanceOf(type, obj, null);
     }
 
-    public static void isInstanceOf(Class<?> type, Object obj, String message, Object... params) {
+    @Deprecated
+    public static void isInstanceOf(@Nullable Class<?> type, Object obj, String message, Object... params) {
         notNull(type, message, params);
         if (!type.isInstance(obj)) {
             throw new SelfConsistentException(format(message, params));
         }
     }
 
-    public static void isAssignable(Class<?> superType, Class<?> subType) {
+    public static void isAssignable(@Nullable Class<?> superType, Class<?> subType) {
         isAssignable(superType, subType, null);
     }
 
-    public static void isAssignable(Class<?> superType, Class<?> subType, String message, Object... params) {
+    public static void isAssignable(@Nullable Class<?> superType, Class<?> subType, String message, Object... params) {
         notNull(superType, message, params);
         if (subType == null || !superType.isAssignableFrom(subType)) {
             throw new SelfConsistentException(format(message, params));
