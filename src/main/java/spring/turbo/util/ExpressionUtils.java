73123032b86d79de40505cf4809229ff1d2aa450
应卓 <yingzhor@gmail.com>
2022-01-02 07:52:51 +0800

    2022-01-02 07:52:49

--- src/main/java/spring/turbo/util/ExpressionUtils.java
+++ src/main/java/spring/turbo/util/ExpressionUtils.java
@@ -13,9 +13,14 @@ import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.SpelParserConfiguration;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.lang.NonNull;
 
 /**
+ * {@code SPEL}相关工具
+ *
  * @author 应卓
+ * @see Expression
+ * @see SpelExpressionParser
  * @since 1.0.0
  */
 @SuppressWarnings("unchecked")
@@ -25,21 +30,39 @@ public final class ExpressionUtils {
             new SpelParserConfiguration(true, true)
     );
 
+    /**
+     * 私有构造方法
+     */
     private ExpressionUtils() {
         super();
     }
 
-    public static <T> T getValue(String expressionString) {
+    /**
+     * 获取表达式的值
+     *
+     * @param expressionString 表达式
+     * @param <T>              表达式值的类型
+     * @return 值
+     */
+    public static <T> T getValue(@NonNull String expressionString) {
         Asserts.hasText(expressionString);
         final Expression expression = EXPRESSION_PARSER.parseExpression(expressionString);
         return (T) expression.getValue();
     }
 
-    public static <T> T getValue(Object rootObject, String expressionString) {
+    /**
+     * 获取表达式的值
+     *
+     * @param rootObj          根值
+     * @param expressionString 表达式
+     * @param <T>              表达式值的类型
+     * @return 值
+     */
+    public static <T> T getValue(@NonNull Object rootObj, @NonNull String expressionString) {
+        Asserts.notNull(rootObj);
         Asserts.hasText(expressionString);
-        Asserts.notNull(rootObject);
         final Expression expression = EXPRESSION_PARSER.parseExpression(expressionString);
-        final StandardEvaluationContext context = new StandardEvaluationContext(rootObject);
+        final StandardEvaluationContext context = new StandardEvaluationContext(rootObj);
         return (T) expression.getValue(context);
     }
 
