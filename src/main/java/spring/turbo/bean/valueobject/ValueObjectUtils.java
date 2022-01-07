6eeaebc78368582ecf66fce039c5743703e5b18d
应卓 <yingzhor@gmail.com>
2022-01-07 10:14:45 +0800

    2022-01-07 10:14:44

--- src/main/java/spring/turbo/bean/valueobject/ValueObjectUtils.java
+++ src/main/java/spring/turbo/bean/valueobject/ValueObjectUtils.java
@@ -9,6 +9,7 @@
 package spring.turbo.bean.valueobject;
 
 import org.springframework.core.annotation.AnnotationAttributes;
+import org.springframework.lang.NonNull;
 import org.springframework.util.ReflectionUtils;
 import spring.turbo.core.AnnotationUtils;
 import spring.turbo.util.Asserts;
@@ -25,17 +26,38 @@ import static spring.turbo.util.StringPool.ANNOTATION_STRING_NULL;
  */
 public final class ValueObjectUtils {
 
+    /**
+     * 私有构造方法
+     */
     private ValueObjectUtils() {
         super();
     }
 
-    public static Map<String, String> getAliases(Class<?> valueObjectType) {
+    /**
+     * 将ValueObject转换为 {@link ValueObjectGetter} 实例
+     *
+     * @param valueObject ValueObject实例 不可为 {@code null}
+     * @return {@link ValueObjectGetter} 实例
+     * @since 1.0.6
+     */
+    public ValueObjectGetter toValueObjectGetter(@NonNull Object valueObject) {
+        return new ValueObjectGetter(valueObject);
+    }
+
+    /**
+     * 获取VO类各个property的别名
+     *
+     * @param valueObjectType VO类型
+     * @return 别名
+     */
+    @NonNull
+    public static Map<String, String> getAliases(@NonNull Class<?> valueObjectType) {
         Asserts.notNull(valueObjectType);
 
         final Map<String, String> map = new HashMap<>();
 
         // 先从type上查找
-        Alias.List listAnnotation = AnnotationUtils.findAnnotation(valueObjectType, Alias.List.class);
+        final Alias.List listAnnotation = AnnotationUtils.findAnnotation(valueObjectType, Alias.List.class);
         if (listAnnotation != null) {
             for (Alias annotation : listAnnotation.value()) {
                 final String from = annotation.from();
@@ -45,7 +67,7 @@ public final class ValueObjectUtils {
                 }
             }
         } else {
-            Alias annotation = AnnotationUtils.findAnnotation(valueObjectType, Alias.class);
+            final Alias annotation = AnnotationUtils.findAnnotation(valueObjectType, Alias.class);
             if (annotation != null) {
                 final String from = annotation.from();
                 final String to = annotation.to();
