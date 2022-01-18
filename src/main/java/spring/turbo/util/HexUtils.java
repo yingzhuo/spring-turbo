6310a7e5b64753b52c0b268d61b407ccb6b9bd26
应卓 <yingzhor@gmail.com>
2022-01-18 13:39:26 +0800

    2022-01-18 13:39:24

--- src/main/java/spring/turbo/util/HexUtils.java
+++ src/main/java/spring/turbo/util/HexUtils.java
@@ -8,20 +8,30 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package spring.turbo.util;
 
+import org.springframework.lang.NonNull;
+
 /**
  * @author 应卓
  * @since 1.0.0
  */
 public final class HexUtils {
 
-    private static final char[] HEX = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            'a', 'b', 'c', 'd', 'e', 'f'};
+    private static final char[] HEX = {
+            '0', '1', '2', '3', '4',
+            '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f'
+    };
 
+    /**
+     * 私有构造方法
+     */
     private HexUtils() {
         super();
     }
 
-    public static char[] encode(byte[] bytes) {
+    public static char[] encode(@NonNull byte[] bytes) {
+        Asserts.notNull(bytes);
+
         final int nBytes = bytes.length;
         char[] result = new char[2 * nBytes];
 
@@ -36,7 +46,9 @@ public final class HexUtils {
         return result;
     }
 
-    public static byte[] decode(CharSequence s) {
+    public static byte[] decode(@NonNull CharSequence s) {
+        Asserts.notNull(s);
+
         int nChars = s.length();
 
         if (nChars % 2 != 0) {
