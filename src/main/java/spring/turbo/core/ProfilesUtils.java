412591bf47e01939e68f0ebf8f34b8178450a4fa
应卓 <yingzhor@gmail.com>
2022-02-07 07:27:01 +0800

    2022-02-07 07:27:00

---
+++ src/main/java/spring/turbo/core/ProfilesUtils.java
@@ -0,0 +1,37 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.core;
+
+import org.springframework.core.env.Profiles;
+import spring.turbo.util.Asserts;
+
+/**
+ * Profiles相关工具
+ *
+ * @author 应卓
+ * @see EnvironmentUtils
+ * @see org.springframework.core.env.Environment
+ * @since 1.0.13
+ */
+public final class ProfilesUtils {
+
+    /**
+     * 私有构造方法
+     */
+    private ProfilesUtils() {
+        super();
+    }
+
+    public static boolean acceptsProfiles(Profiles profiles) {
+        Asserts.notNull(profiles);
+        return SpringUtils.getEnvironment()
+                .acceptsProfiles(profiles);
+    }
+
+}
