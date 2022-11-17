2f059b0ebe6f3676a4a032263a20afd20a887e95
应卓 <yingzhor@gmail.com>
2022-11-17 09:56:36 +0800

    2022-11-17 09:56:33

--- src/main/java/spring/turbo/bean/condition/ConditionalOnTraceDebugMode.java
+++ src/main/java/spring/turbo/bean/condition/ConditionalOnTraceDebugMode.java
@@ -13,12 +13,17 @@ import org.springframework.context.annotation.Conditional;
 import java.lang.annotation.*;
 
 /**
+ * 在trace或debug模式下生效
+ *
  * @author 应卓
  * @see spring.turbo.bean.injection.IsTraceMode
  * @see spring.turbo.bean.injection.IsDebugMode
  * @see spring.turbo.bean.injection.IsTraceOrDebugMode
+ * @see org.springframework.core.env.Environment
+ * @see org.springframework.boot.ApplicationArguments
  * @since 1.3.0
  */
+@Inherited
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
