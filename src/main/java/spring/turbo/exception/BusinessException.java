45709a5d43be47be9b30a7619b9994edc1d15dcb
应卓 <yingzhor@gmail.com>
2022-11-30 10:54:27 +0800

    2022-11-30 10:54:25

--- src/main/java/spring/turbo/exception/BusinessException.java
+++ src/main/java/spring/turbo/exception/BusinessException.java
@@ -8,7 +8,11 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 package spring.turbo.exception;
 
+import org.springframework.context.MessageSourceResolvable;
 import org.springframework.lang.Nullable;
+import spring.turbo.lang.Immutable;
+
+import java.util.Objects;
 
 import static java.util.Optional.ofNullable;
 
@@ -16,16 +20,37 @@ import static java.util.Optional.ofNullable;
  * 业务异常
  *
  * @author 应卓
+ * @see #builder()
+ * @see org.springframework.context.MessageSourceResolvable
+ * @see org.springframework.context.MessageSource
+ * @see org.springframework.context.support.MessageSourceAccessor
+ * @see org.springframework.context.support.ApplicationObjectSupport
  * @since 2.0.1
  */
-public class BusinessException extends RuntimeException {
+@Immutable
+public final class BusinessException extends RuntimeException implements MessageSourceResolvable {
 
-    public static BusinessException of(@Nullable String msg) {
-        return new BusinessException(msg);
-    }
+    @Nullable
+    private final String[] codes;
+    @Nullable
+    private final Object[] arguments;
+    @Nullable
+    private final String defaultMessage;
 
-    public static BusinessException of(@Nullable Throwable ex) {
-        return of(ofNullable(ex).map(Throwable::getMessage).orElse(null));
+    /**
+     * 创建器专用构造方法
+     *
+     * @param codes          I18n codes
+     * @param arguments      I18n arguments
+     * @param defaultMessage I18n defaultMessage
+     * @see org.springframework.context.MessageSource
+     * @see MessageSourceResolvable
+     */
+    BusinessException(@Nullable String[] codes, @Nullable Object[] arguments, @Nullable String defaultMessage) {
+        super(defaultMessage);
+        this.codes = codes;
+        this.arguments = arguments;
+        this.defaultMessage = defaultMessage;
     }
 
     /**
@@ -42,6 +67,38 @@ public class BusinessException extends RuntimeException {
      */
     public BusinessException(@Nullable String message) {
         super(message);
+        this.codes = null;
+        this.arguments = null;
+        this.defaultMessage = message;
+    }
+
+    public static BusinessExceptionBuilder builder() {
+        return new BusinessExceptionBuilder();
+    }
+
+    public static BusinessException of(@Nullable String msg) {
+        return new BusinessException(msg);
+    }
+
+    public static BusinessException of(@Nullable Throwable ex) {
+        return of(ofNullable(ex).map(Throwable::getMessage).orElse(null));
+    }
+
+    @Override
+    @Nullable
+    public String[] getCodes() {
+        return this.codes;
+    }
+
+    @Override
+    @Nullable
+    public Object[] getArguments() {
+        return this.arguments;
+    }
+
+    @Override
+    public String getDefaultMessage() {
+        return Objects.requireNonNullElseGet(this.defaultMessage, () -> Objects.requireNonNullElse(super.getMessage(), null));
     }
 
 }
---
+++ src/main/java/spring/turbo/exception/BusinessExceptionBuilder.java
@@ -0,0 +1,69 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.exception;
+
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import spring.turbo.bean.Builder;
+import spring.turbo.util.Asserts;
+import spring.turbo.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 业务异常的创建器
+ *
+ * @author 应卓
+ * @since 2.0.1
+ */
+public final class BusinessExceptionBuilder implements Builder<BusinessException> {
+
+    private final List<String> codes = new ArrayList<>();
+    private final List<Object> arguments = new ArrayList<>();
+
+    @Nullable
+    private String defaultMessage = null;
+
+    /**
+     * 构造方法
+     */
+    BusinessExceptionBuilder() {
+        super();
+    }
+
+    public BusinessExceptionBuilder codes(String... codes) {
+        CollectionUtils.nullSafeAddAll(this.codes, codes);
+        return this;
+    }
+
+    public BusinessExceptionBuilder arguments(Object... arguments) {
+        CollectionUtils.nullSafeAddAll(this.arguments, arguments);
+        return this;
+    }
+
+    public BusinessExceptionBuilder defaultMessage(@NonNull String message) {
+        Asserts.notNull(message);
+        this.defaultMessage = message;
+        return this;
+    }
+
+    @Override
+    public BusinessException build() {
+        if (codes.isEmpty() && arguments.isEmpty() && defaultMessage != null) {
+            return BusinessException.of(defaultMessage);
+        }
+
+        String[] codesInUse = codes.isEmpty() ? null : codes.toArray(new String[0]);
+        Object[] argumentInUse = arguments.isEmpty() ? null : arguments.toArray(new Object[0]);
+
+        return new BusinessException(codesInUse, argumentInUse, this.defaultMessage);
+    }
+
+}
