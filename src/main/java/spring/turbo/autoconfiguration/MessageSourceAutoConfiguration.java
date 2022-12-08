2fc31320ca7019f78b58cb1608812704d2b929d7
应卓 <yingzhor@gmail.com>
2022-12-08 17:57:56 +0800

    2022-12-08 17:57:54

--- src/main/java/spring/turbo/autoconfiguration/MessageSourceAutoConfiguration.java
+++ src/main/java/spring/turbo/autoconfiguration/MessageSourceAutoConfiguration.java
@@ -9,11 +9,26 @@
 package spring.turbo.autoconfiguration;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.context.MessageSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.Ordered;
+import org.springframework.util.StringUtils;
+import spring.turbo.convention.ExtraMessageSourceBasenameConvention;
+import spring.turbo.util.CollectionUtils;
+import spring.turbo.util.ServiceLoaderUtils;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Locale;
 
 /**
@@ -21,13 +36,64 @@ import java.util.Locale;
  * @since 2.0.3
  */
 @AutoConfiguration
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@EnableConfigurationProperties
 public class MessageSourceAutoConfiguration {
 
-    /**
-     * 构造方法
-     */
-    public MessageSourceAutoConfiguration() {
-        super();
+    @Bean
+    @ConfigurationProperties(prefix = "spring.messages")
+    public MessageSourceProperties messageSourceProperties() {
+        return new MessageSourceProperties();
+    }
+
+    @Bean(name = AbstractApplicationContext.MESSAGE_SOURCE_BEAN_NAME)
+    public MessageSource messageSource(MessageSourceProperties properties) {
+        final var messageSource = new ResourceBundleMessageSource();
+
+        if (StringUtils.hasText(properties.getBasename())) {
+            messageSource.setBasenames(this.mergeBasename(properties));
+        }
+
+        if (properties.getEncoding() != null) {
+            messageSource.setDefaultEncoding(properties.getEncoding().name());
+        }
+
+        messageSource.setFallbackToSystemLocale(properties.isFallbackToSystemLocale());
+        Duration cacheDuration = properties.getCacheDuration();
+        if (cacheDuration != null) {
+            messageSource.setCacheMillis(cacheDuration.toMillis());
+        }
+        messageSource.setAlwaysUseMessageFormat(properties.isAlwaysUseMessageFormat());
+        messageSource.setUseCodeAsDefaultMessage(properties.isUseCodeAsDefaultMessage());
+        return messageSource;
+    }
+
+    private String[] mergeBasename(MessageSourceProperties properties) {
+        var basename1 = StringUtils
+                .commaDelimitedListToStringArray(StringUtils.trimAllWhitespace(properties.getBasename()));
+        var basename2 = getAllExtraBasename();
+
+        var ret = new ArrayList<String>();
+        CollectionUtils.nullSafeAddAll(ret, basename1);
+        CollectionUtils.nullSafeAddAll(ret, basename2);
+        return ret.toArray(new String[0]);
+    }
+
+    public Collection<String> getAllExtraBasename() {
+        final var list = new ArrayList<String>();
+
+        final var services = ServiceLoaderUtils.loadQuietly(ExtraMessageSourceBasenameConvention.class);
+
+        for (var service : services) {
+            if (service != null) {
+                try {
+                    CollectionUtils.nullSafeAddAll(list, service.getExtraMessageSourceBasename());
+                } catch (Throwable ignored) {
+                    // nop
+                }
+            }
+        }
+        return Collections.unmodifiableCollection(list);
     }
 
     @Bean
---
+++ src/main/java/spring/turbo/convention/spi/package-info.java
@@ -0,0 +1,18 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/**
+ * 规约的具体实现
+ */
+@NonNullApi
+@NonNullFields
+package spring.turbo.convention.spi;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
