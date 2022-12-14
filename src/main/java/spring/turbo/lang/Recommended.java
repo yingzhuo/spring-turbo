ef40d7e66b10ceaa2931443d925ae50794a8c408
应卓 <yingzhor@gmail.com>
2022-12-14 08:37:59 +0800

    2022-12-14 08:37:58

---
+++ src/main/java/spring/turbo/lang/Recommended.java
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.lang;
+
+import java.lang.annotation.*;
+
+/**
+ * 推荐使用
+ *
+ * @author 应卓
+ * @since 2.0.4
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({
+        ElementType.TYPE,
+        ElementType.METHOD
+})
+public @interface Recommended {
+}
