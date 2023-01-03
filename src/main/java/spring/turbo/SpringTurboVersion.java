6e32f5c541343c9f495c3e49da9d708641a41dec
应卓 <yingzhor@gmail.com>
2023-01-03 18:20:15 +0800

    2023-01-03 18:20:13

--- src/main/java/spring/turbo/SpringTurboVersion.java
+++ src/main/java/spring/turbo/SpringTurboVersion.java
@@ -19,7 +19,7 @@ public final class SpringTurboVersion {
     /**
      * 本软件版本
      */
-    public static final String VERSION = "2.0.6";
+    public static final String VERSION = "2.0.7";
 
     /**
      * 私有构造方法
