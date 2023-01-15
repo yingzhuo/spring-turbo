eb97d01b44136784b1b98bdc5a162d65df618e75
应卓 <yingzhor@gmail.com>
2023-01-15 21:01:23 +0800

    2023-01-15 21:01:22

---
+++ src/main/java/spring/turbo/bean/injection/ApplicationHome.java
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.bean.injection;
+
+import org.springframework.beans.factory.annotation.Value;
+import spring.turbo.core.env.ApplicationHomeEnvironmentPostProcessor;
+
+import java.lang.annotation.*;
+
+/**
+ * {@code @Value("${spring.application.home}")} 的快捷方式
+ *
+ * @author 应卓
+ * @see Value
+ * @see spring.turbo.core.env.ApplicationHomeEnvironmentPostProcessor
+ * @since 2.0.8
+ */
+@Inherited
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Value(ApplicationHomeEnvironmentPostProcessor.VALUE_ANNOTATION_VALUE)
+public @interface ApplicationHome {
+}
