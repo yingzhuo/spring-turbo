99d055d54281ed8d510896aa950cffa9d206b9a6
应卓 <yingzhor@gmail.com>
2023-04-18 06:55:31 +0800

    2023-04-18 06:55:29

---
+++ src/main/java/spring/turbo/bean/jsr380/Base64StringValidator.java
@@ -0,0 +1,33 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.bean.jsr380;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.lang.Nullable;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author 应卓
+ * @since 2.2.2
+ */
+public class Base64StringValidator implements ConstraintValidator<Base64String, String> {
+
+    @Override
+    public boolean isValid(@Nullable String value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+
+        var pattern = Pattern.compile("^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$");
+        return pattern.matcher(value).find();
+    }
+
+}
