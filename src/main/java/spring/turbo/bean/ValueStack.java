a9a360504d29b9d1f81ce64b76f1fc7e2e0ec0b4
应卓 <yingzhor@gmail.com>
2023-05-03 17:04:31 +0800

    2023-05-03 17:04:30

--- src/main/java/spring/turbo/bean/ValueStack.java
+++ src/main/java/spring/turbo/bean/ValueStack.java
@@ -11,8 +11,13 @@ package spring.turbo.bean;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
+import java.util.Objects;
+
 /**
+ * 值栈，建议配合 {@code SpEL} 使用
+ *
  * @author 应卓
+ * @see org.springframework.beans.factory.annotation.Value
  * @since 2.2.4
  */
 public sealed interface ValueStack permits ValueStackImpl {
@@ -25,6 +30,11 @@ public sealed interface ValueStack permits ValueStackImpl {
         return findString(environmentName, resourceLocation, null);
     }
 
+    public default String findRequiredString(@Nullable String environmentName, @Nullable String resourceLocation, @Nullable String defaultValue) {
+        var value = findString(environmentName, resourceLocation, defaultValue);
+        return Objects.requireNonNull(value);
+    }
+
     // -----------------------------------------------------------------------------------------------------------------
 
     @Nullable
@@ -33,6 +43,11 @@ public sealed interface ValueStack permits ValueStackImpl {
     @Nullable
     public default <T> T findObject(@NonNull Class<T> valueType, @Nullable String environmentName, @Nullable String resourceLocation) {
         return findObject(valueType, environmentName, resourceLocation, null);
+    }
+
+    public default <T> T findRequiredObject(@NonNull Class<T> valueType, @Nullable String environmentName, @Nullable String resourceLocation, @Nullable String defaultValue) {
+        var value = findObject(valueType, environmentName, resourceLocation, defaultValue);
+        return Objects.requireNonNull(value);
     }
 
 }
