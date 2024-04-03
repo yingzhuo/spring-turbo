25f9ec0d465a1f3d86a3f7144102aa3c850d463f
应卓 <yingzhor@gmail.com>
2024-04-03 09:03:50 +0800

    2024-04-03 09:03:46

---
+++ src/main/java/spring/turbo/util/DistanceUtils.java
@@ -0,0 +1,83 @@
+package spring.turbo.util;
+
+import static java.lang.Math.*;
+
+/**
+ * 基于经纬度的距离计算工具
+ *
+ * @author 应卓
+ *
+ * @since 3.2.4
+ */
+public final class DistanceUtils {
+
+    /**
+     * 计算两个点之间的距离
+     *
+     * @param lat1
+     *            第一点纬度
+     * @param lon1
+     *            第一点经度
+     * @param lat2
+     *            第二点纬度
+     * @param lon2
+     *            第二点经度
+     * @param unit
+     *            距离单位
+     *
+     * @return 距离
+     *
+     * @see DistanceUtils.Unit
+     */
+    public static double distance(double lat1, double lon1, double lat2, double lon2, Unit unit) {
+        double theta = lon1 - lon2;
+        double dist = sin(deg2rad(lat1)) * sin(deg2rad(lat2))
+                + cos(deg2rad(lat1)) * cos(deg2rad(lat2)) * cos(deg2rad(theta));
+        dist = acos(dist);
+        dist = rad2deg(dist);
+        dist = dist * 60 * 1.1515;
+
+        return switch (unit) {
+        case MILES -> dist;
+        case KILOMETERS -> dist * 1.609344;
+        case NAUTICAL_MILES -> dist * 0.8684;
+        };
+    }
+
+    private static double deg2rad(double deg) {
+        return (deg * PI / 180.0);
+    }
+
+    private static double rad2deg(double rad) {
+        return (rad * 180.0 / PI);
+    }
+
+    /**
+     * 距离单位
+     */
+    public static enum Unit {
+
+        /**
+         * 英里
+         */
+        MILES,
+
+        /**
+         * 千米
+         */
+        KILOMETERS,
+
+        /**
+         * 海里
+         */
+        NAUTICAL_MILES
+    }
+
+    /**
+     * 私有构造方法
+     */
+    private DistanceUtils() {
+        super();
+    }
+
+}
