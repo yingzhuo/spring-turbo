b8bc2631bf200188e716ebc258adf8c02c5ad7ac
应卓 <yingzhor@gmail.com>
2024-06-03 09:55:04 +0800

    2024-06-03 09:55:01

---
+++ src/main/java/spring/turbo/util/XmlStringUtils.java
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.util;
+
+/**
+ * @author 应卓
+ *
+ * @see JsonStringUtils
+ *
+ * @since 3.3.0
+ */
+public final class XmlStringUtils {
+
+    public static String removeExtraWhiteSpaces(String xmlString) {
+        return xmlString.replaceAll(">[\\s\r\n]*<", "><");
+    }
+
+    /**
+     * 私有构造方法
+     */
+    private XmlStringUtils() {
+        super();
+    }
+}
