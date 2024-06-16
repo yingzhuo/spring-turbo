12172678b320624d000f3ad19ac8d91ab9d7fa7e
应卓 <yingzhor@gmail.com>
2024-06-17 05:18:00 +0800

    2024-06-17 05:17:56

---
+++ src/main/java/spring/turbo/util/security/KeyStoreType.java
@@ -0,0 +1,32 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.util.security;
+
+/**
+ * KeyStore格式类型
+ *
+ * @author 应卓
+ *
+ * @since 3.3.0
+ */
+public enum KeyStoreType {
+
+    JKS("JKS"), PKCS12("pkcs12");
+
+    private final String value;
+
+    private KeyStoreType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
