c04cf8b856235e21ac5f5fff065debb8880cda08
应卓 <yingzhor@gmail.com>
2024-06-20 09:19:14 +0800

    2024-06-20 09:19:13

---
+++ src/main/java/spring/turbo/util/function/GenericGenerator.java
@@ -0,0 +1,26 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *    ____             _            _____           _
+ *   / ___| _ __  _ __(_)_ __   __ |_   _|   _ _ __| |__   ___
+ *   \___ \| '_ \| '__| | '_ \ / _` || || | | | '__| '_ \ / _ \
+ *    ___) | |_) | |  | | | | | (_| || || |_| | |  | |_) | (_) |
+ *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
+ *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+package spring.turbo.util.function;
+
+/**
+ * @author 应卓
+ * @since 3.3.1
+ */
+@FunctionalInterface
+@SuppressWarnings("rawtypes")
+public interface GenericGenerator extends java.util.function.Supplier {
+
+    @Override
+    public default Object get() {
+        return generate();
+    }
+
+    public Object generate();
+
+}
