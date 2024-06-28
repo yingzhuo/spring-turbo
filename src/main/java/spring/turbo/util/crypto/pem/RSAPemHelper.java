0a8252e27232899c40da0bcac8b5f00fff9ba4b2
应卓 <yingzhor@gmail.com>
2024-06-28 19:44:38 +0800

    2024-06-28 19:44:34

--- src/main/java/spring/turbo/util/crypto/pem/RSAPemHelper.java
+++ src/main/java/spring/turbo/util/crypto/pem/RSAPemHelper.java
@@ -6,15 +6,16 @@
  *   |____/| .__/|_|  |_|_| |_|\__, ||_| \__,_|_|  |_.__/ \___/
  *         |_|                 |___/   https://github.com/yingzhuo/spring-turbo
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-package spring.turbo.util.crypto;
+package spring.turbo.util.crypto.pem;
 
 import org.bouncycastle.util.io.pem.PemReader;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UncheckedIOException;
 import java.security.KeyFactory;
+import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -22,13 +23,16 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
+import static spring.turbo.io.IOExceptionUtils.toUnchecked;
+
 /**
- * 这是一个小工具。可以从PEM文件中读取RSA公钥和私钥
+ * 这是一个小工具。可以从PEM文件中读取RSA公钥和私钥 <br>
+ * 此工具依赖 <a href="https://search.maven.org/search?q=bcprov-jdk15to18">BouncyCastle</a>
  *
  * <pre>
  *     openssl genrsa -out keypair.pem 2048
- *     openssl rsa -in keypair.pem -pubout -inform PEM -outform PEM -out public-key.pem
- *     openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private-key.pem
+ *     openssl rsa -in keypair.pem -pubout -inform PEM -outform PEM -out rsa-public-key.pem
+ *     openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out rsa-private-key.pem
  * </pre>
  *
  * @author 应卓
@@ -43,6 +47,21 @@ public final class RSAPemHelper {
      */
     private RSAPemHelper() {
         super();
+    }
+
+    public static KeyPair readKeyPair(Resource publicKeyResource, Resource privateKeyResource) {
+        return new KeyPair(
+                readX509PublicKey(publicKeyResource),
+                readPKCS8PrivateKey(privateKeyResource)
+        );
+    }
+
+    public static RSAPublicKey readX509PublicKey(Resource resource) {
+        try {
+            return readX509PublicKey(resource.getInputStream());
+        } catch (IOException e) {
+            throw toUnchecked(e);
+        }
     }
 
     public static RSAPublicKey readX509PublicKey(InputStream inputStream) {
@@ -60,9 +79,17 @@ public final class RSAPemHelper {
             var spec = new X509EncodedKeySpec(content);
             return (RSAPublicKey) factory.generatePublic(spec);
         } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            throw toUnchecked(e);
         } catch (InvalidKeySpecException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static RSAPrivateKey readPKCS8PrivateKey(Resource resource) {
+        try {
+            return readPKCS8PrivateKey(resource.getInputStream());
+        } catch (IOException e) {
+            throw toUnchecked(e);
         }
     }
 
@@ -81,7 +108,7 @@ public final class RSAPemHelper {
             var spec = new PKCS8EncodedKeySpec(content);
             return (RSAPrivateKey) factory.generatePrivate(spec);
         } catch (IOException e) {
-            throw new UncheckedIOException(e);
+            throw toUnchecked(e);
         } catch (InvalidKeySpecException e) {
             throw new RuntimeException(e);
         }
