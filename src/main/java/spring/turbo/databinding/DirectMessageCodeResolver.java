f10696c8e2dd985bc4ea92a8b31e8628d2100a8e
应卓 <yingzhor@gmail.com>
2024-07-08 08:20:51 +0800

    2024-07-08 08:20:50

---
+++ src/main/java/spring/turbo/databinding/DirectMessageCodeResolver.java
@@ -0,0 +1,50 @@
+package spring.turbo.databinding;
+
+import org.springframework.validation.MessageCodesResolver;
+
+/**
+ * 简易{@link MessageCodesResolver} 实现。
+ *
+ * @author 应卓
+ * @see #getInstance()
+ * @since 3.3.1
+ */
+public final class DirectMessageCodeResolver implements MessageCodesResolver {
+
+    /**
+     * 私有构造方法
+     */
+    private DirectMessageCodeResolver() {
+    }
+
+    /**
+     * 获取本类型单例
+     *
+     * @return 单例实例
+     */
+    public static DirectMessageCodeResolver getInstance() {
+        return SyncAvoid.INSTANCE;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String[] resolveMessageCodes(String errorCode, String objectName) {
+        return new String[]{errorCode};
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String[] resolveMessageCodes(String errorCode, String objectName, String field, Class<?> fieldType) {
+        return new String[]{errorCode};
+    }
+
+    // 延迟加载
+    private static class SyncAvoid {
+        private static final DirectMessageCodeResolver INSTANCE = new DirectMessageCodeResolver();
+    }
+
+}
