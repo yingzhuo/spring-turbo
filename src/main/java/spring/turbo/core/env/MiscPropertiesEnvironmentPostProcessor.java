8661db6e5ae698b42dd3eafb54d5c8df630ac338
应卓 <yingzhor@gmail.com>
2024-07-10 09:13:17 +0800

    2024-07-10 09:13:16

--- src/main/java/spring/turbo/core/env/MiscPropertiesEnvironmentPostProcessor.java
+++ src/main/java/spring/turbo/core/env/MiscPropertiesEnvironmentPostProcessor.java
@@ -8,28 +8,36 @@ import org.springframework.core.env.MapPropertySource;
 import spring.turbo.util.CurrentProcess;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 
 /**
  * 杂项设置
  *
  * @author 应卓
  * @see spring.turbo.bean.injection.ApplicationHome
+ * @see spring.turbo.bean.injection.ApplicationPid
+ * @see spring.turbo.bean.injection.ApplicationParentPid
+ * @see spring.turbo.bean.injection.ApplicationProcessUser
  * @since 3.3.1
  */
 public class MiscPropertiesEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
-        final Map<String, Object> stringObjectMap = new HashMap<>();
-        stringObjectMap.put("spring.application.home", SpringApplicationHolders.getApplicationHome().toString());
-        stringObjectMap.put("spring.process.pid", CurrentProcess.pid());
-        stringObjectMap.put("spring.process.parent-pid", CurrentProcess.parentPid());
-        stringObjectMap.put("spring.process.user", CurrentProcess.user());
+        final var variables = new HashMap<String, Object>();
+        variables.put("spring.process.pid", CurrentProcess.pid());
+        variables.put("spring.process.parent-pid", CurrentProcess.parentPid());
+        variables.put("spring.process.user", CurrentProcess.user());
+
+        Optional.ofNullable(SpringApplicationHolders.getApplicationHome()).ifPresent(
+                home -> {
+                    variables.put("spring.application.home", home.toAbsolutePath().toString());
+                }
+        );
 
         var propertySource = new MapPropertySource(
                 "spring-turbo-misc",
-                stringObjectMap
+                variables
         );
 
         environment.getPropertySources()
