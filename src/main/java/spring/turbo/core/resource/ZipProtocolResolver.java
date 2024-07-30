cb3237e5f03be294f342cf976564a15c57fe6d6b
应卓 <yingzhor@gmail.com>
2024-07-30 10:34:11 +0800

    2024-07-30 10:34:08

---
+++ src/main/java/spring/turbo/core/resource/ZipProtocolResolver.java
@@ -0,0 +1,53 @@
+package spring.turbo.core.resource;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ProtocolResolver;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.zip.ZipFile;
+
+/**
+ * @author 应卓
+ * @see JarProtocolResolver
+ * @since 3.4.0
+ */
+final class ZipProtocolResolver implements ProtocolResolver {
+
+    private static final String ZIP_PREFIX = "zip:";
+
+    @Nullable
+    @Override
+    public Resource resolve(String location, ResourceLoader resourceLoader) {
+        if (StringUtils.startsWithIgnoreCase(location, ZIP_PREFIX)) {
+
+            var parts = StringUtils.delimitedListToStringArray(location.substring(ZIP_PREFIX.length()), "!");
+            if (parts.length == 2) {
+                var zipLocation = parts[0];
+                var entryName = parts[1];
+
+                try {
+                    return doResolve(zipLocation, entryName, resourceLoader);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private Resource doResolve(String zipLocation, String entryName, ResourceLoader resourceLoader) throws IOException {
+        var zipResource = resourceLoader.getResource(zipLocation);
+        try (var zipFile = new ZipFile(zipResource.getFile())) {
+            var zipEntry = zipFile.getEntry(entryName);
+            return new ByteArrayResource(StreamUtils.copyToByteArray(zipFile.getInputStream(zipEntry)));
+        }
+    }
+
+}
