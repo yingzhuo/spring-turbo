0fff608844105bc2cac34f9939f517dabb5d20c3
应卓 <yingzhor@gmail.com>
2024-07-30 19:45:58 +0800

    2024-07-30 19:45:57

---
+++ src/main/java/spring/turbo/core/resource/AbstractZipProtocolResolver.java
@@ -0,0 +1,58 @@
+package spring.turbo.core.resource;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ProtocolResolver;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.lang.Nullable;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.zip.ZipFile;
+
+/**
+ * @author 应卓
+ * @see ZipProtocolResolver
+ * @see JarProtocolResolver
+ * @since 3.4.0
+ */
+abstract class AbstractZipProtocolResolver implements ProtocolResolver {
+
+    private final String prefix;
+
+    public AbstractZipProtocolResolver(String prefix) {
+        this.prefix = prefix;
+    }
+
+    @Nullable
+    @Override
+    public final Resource resolve(String location, ResourceLoader resourceLoader) {
+        if (StringUtils.startsWithIgnoreCase(location, prefix)) {
+
+            var parts = StringUtils.delimitedListToStringArray(location.substring(prefix.length()), "!");
+            if (parts.length == 2) {
+                var zipLocation = parts[0];
+                var entryName = parts[1];
+
+                try {
+                    return doResolve(zipLocation, entryName, resourceLoader);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private Resource doResolve(String zipLocation, String entryName, ResourceLoader resourceLoader) throws IOException {
+        var zipResource = resourceLoader.getResource(zipLocation);
+        try (var zipFile = new ZipFile(zipResource.getFile())) {
+            var zipEntry = zipFile.getEntry(entryName);
+            return new ByteArrayResource(StreamUtils.copyToByteArray(zipFile.getInputStream(zipEntry)));
+        }
+    }
+
+}
