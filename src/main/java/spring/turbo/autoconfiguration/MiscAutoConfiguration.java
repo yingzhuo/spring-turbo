ecd1efc736d9d9c3646c24ac3fa5dc673ecf6c04
应卓 <yingzhor@gmail.com>
2024-08-12 08:29:28 +0800

    2024-08-12 08:29:27

--- src/main/java/spring/turbo/autoconfiguration/MiscAutoConfiguration.java
+++ src/main/java/spring/turbo/autoconfiguration/MiscAutoConfiguration.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import spring.turbo.util.crypto.keystore.KeyStoreFormatConverter;
 
+/**
+ * @author 应卓
+ * @since 3.3.2
+ */
 @AutoConfiguration
 public class MiscAutoConfiguration {
 
