b4c1b3f093866d0a65b00c4290ae49bc7d704d4d
应卓 <yingzhor@gmail.com>
2024-11-06 14:04:35 +0800

    2024-11-06 14:04:34

--- src/main/java/spring/turbo/bean/classpath/ClassPathScannerBuilder.java
+++ src/main/java/spring/turbo/bean/classpath/ClassPathScannerBuilder.java
@@ -31,7 +31,7 @@ public final class ClassPathScannerBuilder {
     private final List<TypeFilter> includeFilters = new LinkedList<>();
     private final List<TypeFilter> excludeFilters = new LinkedList<>();
     private Environment environment = new StandardEnvironment();
-    private ResourceLoader resourceLoader = new ApplicationResourceLoader();
+    private ResourceLoader resourceLoader = ApplicationResourceLoader.get();
     private ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
 
     /**
